8c3c26ec48f34a6e76b3b1e2f65bc11d8f5944a6
Juan Camilo Garcia Cuevas <juanca.cuevas.05@outlook.com>
2024-10-17 22:52:39 +0200

    Last changes for gte the 100% mades

--- src/main/java/org/factoriaf5/fizzbuzz/FizzbuzzLogic.java
+++ src/main/java/org/factoriaf5/fizzbuzz/FizzbuzzLogic.java
@@ -1,7 +1,15 @@
 package org.factoriaf5.fizzbuzz;
 
 public class FizzbuzzLogic {
-    public String Fizz(String fizz) {
-        return "fizz";
+    public String FizzBuzz(Integer numero) {
+        String numeroCadenaString = numero.toString();
+        if (numero % 3 == 0 && numero % 5 == 0) {
+            return "fizzbuzz";
+        } else if (numero % 3 == 0 || numeroCadenaString.contains("3")) {
+            return "fizz";
+        } else if (numero % 5 == 0 || numeroCadenaString.contains("5") ) {
+            return "buzz";
+        }
+        return String.valueOf(numero);
     }
 }
--- src/test/java/org/factoriaf5/fizzbuzz/FizzbuzzLogicTest.java
+++ src/test/java/org/factoriaf5/fizzbuzz/FizzbuzzLogicTest.java
@@ -1,5 +1,47 @@
 package org.factoriaf5.fizzbuzz;
 
-public class FizzbuzzLogicTest {
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+public class FizzbuzzLogicTest {
+    private FizzbuzzLogic metodo;
+
+    @BeforeEach
+    public void init () {
+        this.metodo = new FizzbuzzLogic();
+    }
+
+    @Test
+    public void numeroToString () {
+        assertEquals("4", metodo.FizzBuzz(4));
+        assertEquals("1", metodo.FizzBuzz(1));
+    }
+
+    @Test
+    public void fizzbuzz () {
+        assertEquals("fizzbuzz", metodo.FizzBuzz(15));
+        assertTrue(metodo.FizzBuzz(45) == "fizzbuzz");
+        assertFalse(metodo.FizzBuzz(12) == "fizzbuzz");
+        assertFalse(metodo.FizzBuzz(25) == "fizzbuzz");
+    }
+
+    @Test
+    public void fizz () {
+        assertEquals("fizz", metodo.FizzBuzz(12));
+        assertTrue(metodo.FizzBuzz(3) == "fizz");
+        assertFalse(metodo.FizzBuzz(14) == "fizz");
+        assertFalse(metodo.FizzBuzz(9) == "buzz");
+        assertInstanceOf(String.class, metodo.FizzBuzz(43));
+    }
+
+    @Test
+    public void buzz () {
+        assertEquals("buzz", metodo.FizzBuzz(20));
+        assertTrue(metodo.FizzBuzz(5) == "buzz");
+        assertTrue(metodo.FizzBuzz(56) == "buzz");
+        assertFalse(metodo.FizzBuzz(13) == "buzz");
+        assertFalse(metodo.FizzBuzz(10) == "fizz");
+        assertInstanceOf(String.class, metodo.FizzBuzz(45));
+    }
 }
